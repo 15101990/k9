4d74d4e5669fdbce62b6166f1e8436b54995fae1
151090 <m.helmy.amit@gmail.com>
2021-06-11 16:52:22 +0200

    note

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+                       //poor readability
+
+int main()
+{
+    char button,lamp;
+    if(button == 0)    //active low pin (the button is pressed)
+    lamp=1;
+
+    if(button==1)
+    lamp=0;
+    return 0;
+}
